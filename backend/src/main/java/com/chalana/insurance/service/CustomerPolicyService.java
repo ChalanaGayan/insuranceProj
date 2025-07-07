2d81811d35f6e6bee81d4bb87cbffe087e43ef2c
Dhanawardhana G.A.C.G. <109666856+ChalanaGayan@users.noreply.github.com>
2025-07-07 21:32:04 +0530

    Add CustomerPolicyService to handle policy enrollment and retrieval for users

---
+++ backend/src/main/java/com/chalana/insurance/service/CustomerPolicyService.java
@@ -0,0 +1,45 @@
+package com.chalana.insurance.service;
+
+
+import com.chalana.insurance.model.CustomerPolicy;
+import com.chalana.insurance.model.Policy;
+import com.chalana.insurance.model.User;
+import com.chalana.insurance.repository.CustomerPolicyRepository;
+import com.chalana.insurance.repository.PolicyRepository;
+import com.chalana.insurance.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CustomerPolicyService {
+
+    private final CustomerPolicyRepository customerPolicyRepository;
+    private final PolicyRepository policyRepository;
+    private final UserRepository userRepository;
+
+
+    public void enrollPolicy(String username, Long policyID) {
+
+        User customer = userRepository.findByUsername(username)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        Policy policy = policyRepository.findById(policyID)
+                .orElseThrow(() -> new RuntimeException("Policy not found"));
+
+        CustomerPolicy cp = new CustomerPolicy();
+        cp.setCustomer(customer);
+        cp.setPolicy(policy);
+
+        customerPolicyRepository.save(cp);
+    }
+
+    public List<CustomerPolicy> getCustomerPolicies(String username) {
+        User customer = userRepository.findByUsername(username)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        return customerPolicyRepository.findByCustomer(customer);
+    }
+}
